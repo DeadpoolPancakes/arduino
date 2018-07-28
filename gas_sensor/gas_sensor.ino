1112e3e88cebd768b5624d104c794a9dde398e37
Jamie <jamie@naylorwebsites.co.uk>
2018-07-28 13:51:10 +0100

    basic gas sensor code

---
+++ gas_sensor/gas_sensor.ino
@@ -0,0 +1,14 @@
+int smokeD0 = A5;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(smokeD0, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int smoke = analogRead(smokeD0);
+  Serial.print("Pin A0: ");
+  Serial.println(smoke);
+  delay(1000);
+}
